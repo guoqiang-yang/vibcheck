94604cca992e980f1fe210817c18f9b0901e4c19
yanggq_hkr <Qiang00789#>
2026-06-23 11:17:00 +0800

    bugfix

--- frontend/src/pages/LedgerManage.tsx
+++ frontend/src/pages/LedgerManage.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { api } from '../api/client'
-import type { BillCategory, Project, ProjectCreate, ProjectUpdate } from '../types'
+import type { Project, ProjectCreate, ProjectUpdate } from '../types'
 
 const inputStyle: React.CSSProperties = {
   width: '100%', boxSizing: 'border-box', padding: '9px 12px',
